78b750b60248aae047f8e42f6e7ef4a07b42a21c
Arsham Shirvani <arshamshirvani@gmail.com>
2021-12-20 20:50:47 +0000

    fix: tab in insert mode

--- lua/settings/cmp.lua
+++ lua/settings/cmp.lua
@@ -68,15 +68,19 @@ cmp.setup({
         }),
 
         -- only use < tab >/<s-tab> for switching between placeholders.
-        ["<Tab>"] = cmp.mapping(function()
+        ["<Tab>"] = cmp.mapping(function(fallback)
             if vim.fn["vsnip#available"](1) == 1 then
                 feedkey("<Plug>(vsnip-expand-or-jump)", "")
+            else
+                fallback()
             end
         end, { "i", "s" }),
 
-        ["<S-Tab>"] = cmp.mapping(function()
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
             if vim.fn["vsnip#jumpable"](-1) == 1 then
                 feedkey("<Plug>(vsnip-jump-prev)", "")
+            else
+                fallback()
             end
         end, { "i", "s" }),
     },
