62bbeb758bb3cf136be6b1cb30e670556c32f6ed
Arsham Shirvani <arshamshirvani@gmail.com>
2021-12-24 21:20:30 +0000

    refactor: autopair rules for lua

--- lua/settings/init.lua
+++ lua/settings/init.lua
@@ -33,17 +33,17 @@ function M.autopairs()
     local Rule = require('nvim-autopairs.rule')
     local cmp_autopairs = require('nvim-autopairs.completion.cmp')
     local cmp = require('cmp')
-    autopairs.setup{
-        -- autopairs = {enable = true},
-        check_ts = true,
-    }
 
+    autopairs.setup{
+        check_ts = true,
+        -- will ignore alphanumeric and `.` symbol
+        ignored_next_char = "[%w%.]",
+    }
+    autopairs.add_rules(require('nvim-autopairs.rules.endwise-lua'))
     -- press % => %% only while inside a comment or string
     autopairs.add_rules({
-        Rule("%", "%", "lua")
+        Rule("|", "|", "lua")
             :with_pair(ts_conds.is_ts_node({'string','comment'})),
-        Rule("$", "$", "lua")
-            :with_pair(ts_conds.is_not_ts_node({'function'}))
     })
 
     cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
