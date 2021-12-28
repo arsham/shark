c0ecc3f91da238b04ce5f6b7c83625c8f9c76c4b
Arsham Shirvani <arshamshirvani@gmail.com>
2021-12-28 17:19:05 +0000

    refactor: simplify yanker logic

--- lua/yanker.lua
+++ lua/yanker.lua
@@ -1,57 +1,51 @@
 local util = require('util')
 require('astronauta.keymap')
 
-local store = {}
-__Clipboard_storage = __Clipboard_storage or {}
+local store = _t()
+__Clipboard_storage = __Clipboard_storage or _t()
 store = __Clipboard_storage
 
 util.augroup{"CLIPBOARD", {
     {"TextYankPost", "*", function()
-        -- We are trying to make a set here, instead we will clean up when we
-        -- read.
         table.insert(store, 1, vim.v.event)
     end},
 }}
 
----Removes duplicates from the store. We do this just when we list the items,
----otherwise we have to spend too much time deduplicating everytime we yank
----something.
-local function make_unique()
-    local unique = {}
-    local seen = {}
-    for _, v in pairs(store) do
-        local key = table.concat(v.regcontents, "\n"):gsub('%s+', '')
-        if key == '' then
-        elseif seen[key] == nil then
-            seen[key] = true
-            table.insert(unique, v)
-        end
-    end
-    store = unique
-    __Clipboard_storage = unique
-end
-
-util.highlight("YankerEmptySpace", {guibg = 'red'})
-
 ---Lists all yank history, and will set it to the unnamed register on
 ---selection.
 vim.keymap.nnoremap{'<leader>yh', silent=true, function()
-    make_unique()
-    local yank_list = {}
-    for i, v in pairs(store) do
-        local value = table.concat(v.regcontents, '<CR>')
+    local yank_list = _t()
+    local seen = _t()
+    store:filter(function(v)
+        local key = table.concat(v.regcontents, "\n"):gsub('%s+', '')
+        if seen[key] then
+            return false
+        end
+        seen[key] = true
+        return true
+    end)
+    :map(function(v, i)
+        local value = table.concat(v.regcontents, util.ansi_color(util.colours.blue, '<CR>'))
         local type = "BLOCK"
         if v.regtype == "v" then
             type = "visual"
         elseif v.regtype == "V" then
             type = "VISUAL"
         end
-        table.insert(yank_list, i .. '\t' .. type .. '\t' .. value)
-    end
+        table.insert(yank_list, string.format('%d\t%s\t%s', i, util.ansi_color(util.colours.green, type), value))
+    end)
+
+    if yank_list:length() == 0 then return end
 
     local wrapped = vim.fn["fzf#wrap"]({
         source = yank_list,
-        options = [[--prompt "Preciously Yanked > " -d '\t' --with-nth 2.. -n 2,2..3]],
+        options = table.concat({
+            '--prompt "Preciously Yanked > "',
+            '--ansi',
+            '-d "\t"',
+            '--with-nth 2..',
+            '--nth 2,2..3',
+        }, ' ')
     })
     wrapped["sink*"] = function(line)
         local index = string.gmatch(line[2], '%d+')()
