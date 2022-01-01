b0e627d79f94df0db81cbdf0e533b7e695ea0868
Arsham Shirvani <arshamshirvani@gmail.com>
2022-01-01 20:56:35 +0000

    add: support for multiple palettes

--- lua/visuals.lua
+++ lua/visuals.lua
@@ -1,4 +1,4 @@
 vim.opt.background = 'dark'
 vim.opt.guifont = 'DejaVuSansMono Nerd Font:h10'
 ---Defer setting the colorscheme until the UI loads.
-require('util').autocmd{'UIEnter', '*', 'colorscheme arsham'}
+require('util').autocmd{'UIEnter', '*', 'colorscheme arsham_light'}
