32a2b6e4961eff5716877b8eb92b04e10688e58b
Arsham Shirvani <arshamshirvani@gmail.com>
2022-01-03 19:42:11 +0000

    feat: json fold on indent

--- after/ftplugin/json.lua
+++ after/ftplugin/json.lua
@@ -3,5 +3,6 @@ if vim.fn.getbufvar(bufname, 'ftplugin_loaded') == true then return end
 vim.fn.setbufvar(bufname, 'ftplugin_loaded', true)
 
 vim.bo.softtabstop = 2
-vim.bo.tabstop = 2
-vim.bo.shiftwidth = 2
+vim.bo.tabstop     = 2
+vim.bo.shiftwidth  = 2
+vim.wo.foldmethod  = 'indent'
