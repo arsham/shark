086d6376fb903524363c39941713c7611b37e8da
Arsham Shirvani <arshamshirvani@gmail.com>
2022-01-03 19:40:16 +0000

    add: autopairs treesitter rule

--- lua/settings/autopairs.lua
+++ lua/settings/autopairs.lua
@@ -1,5 +1,4 @@
 local autopairs = require('nvim-autopairs')
-local ts_conds = require('nvim-autopairs.ts-conds')
 local Rule = require('nvim-autopairs.rule')
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require('cmp')
@@ -8,7 +7,12 @@ autopairs.setup{
   check_ts = true,
   --- will ignore alphanumeric and `.` symbol
   ignored_next_char = "[%w%.]",
+  ts_config = {
+    lua = { 'string' },
+  },
 }
+
+local ts_conds = require('nvim-autopairs.ts-conds')
 autopairs.add_rules(require('nvim-autopairs.rules.endwise-lua'))
 --- press % => %% only while inside a comment or string
 autopairs.add_rules({
