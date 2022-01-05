895acc707943fc83faffaaf289cd323fa626c237
Arsham Shirvani <arshamshirvani@gmail.com>
2022-01-05 10:00:06 +0000

    feat: add stylua formatter

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,19 +1,27 @@
-local util = require('util')
+local util = require("util")
 local M = {}
 
 util.command("Filename", function()
-  vim.notify(vim.fn.expand '%:p', vim.lsp.log_levels.INFO, {
-    title="Filename",
-    timeout=3000,
+  vim.notify(vim.fn.expand("%:p"), vim.lsp.log_levels.INFO, {
+    title = "Filename",
+    timeout = 3000,
   })
 end)
-util.command("YankFilename",  function() vim.fn.setreg('"', vim.fn.expand '%:t') end)
-util.command("YankFilenameC", function() vim.fn.setreg('+', vim.fn.expand '%:t') end)
-util.command("YankFilepath",  function() vim.fn.setreg('"', vim.fn.expand '%:p') end)
-util.command("YankFilepathC", function() vim.fn.setreg('+', vim.fn.expand '%:p') end)
+util.command("YankFilename", function()
+  vim.fn.setreg('"', vim.fn.expand("%:t"))
+end)
+util.command("YankFilenameC", function()
+  vim.fn.setreg("+", vim.fn.expand("%:t"))
+end)
+util.command("YankFilepath", function()
+  vim.fn.setreg('"', vim.fn.expand("%:p"))
+end)
+util.command("YankFilepathC", function()
+  vim.fn.setreg("+", vim.fn.expand("%:p"))
+end)
 
 util.command("MergeConflict", ":grep '<<<<<<< HEAD'")
-util.command("JsonDiff",      [[vert ball | windo execute '%!gojq' | windo diffthis]])
+util.command("JsonDiff", [[vert ball | windo execute '%!gojq' | windo diffthis]])
 
 ---Sets up a watch on the filename if it is a lua module.
 ---@param filenames string[]
@@ -23,9 +31,9 @@ local function setup_watch(filenames)
     local mod, ok = util.file_module(filename)
 
     if not ok then
-      local msg = string.format('Could not figure out the package: %s', filename)
+      local msg = string.format("Could not figure out the package: %s", filename)
       vim.notify(msg, vim.lsp.log_levels.ERROR, {
-        title = 'Reload Error',
+        title = "Reload Error",
         timeout = 2000,
       })
     else
@@ -36,52 +44,57 @@ local function setup_watch(filenames)
 end
 
 local bufname = vim.fn.bufname()
-if vim.fn.getbufvar(bufname, 'watch_lua_file_augroup') ~= true then
-  vim.fn.setbufvar(bufname, 'watch_lua_file_augroup', true)
-  util.augroup{"WATCH_LUA_FILE"}
+if vim.fn.getbufvar(bufname, "watch_lua_file_augroup") ~= true then
+  vim.fn.setbufvar(bufname, "watch_lua_file_augroup", true)
+  util.augroup({ "WATCH_LUA_FILE" })
 end
 
 function M.watch_file_changes(filenames)
-  local modules= setup_watch(filenames)
+  local modules = setup_watch(filenames)
   local names = {}
   for _, module in ipairs(modules) do
     table.insert(names, module.name)
 
-    util.autocmd{"WATCH_LUA_FILE BufWritePost", module.name, run=function()
-      for _, mod in ipairs(modules) do
-        package.loaded[mod.module] = nil
-        -- selene: allow(global_usage)
-        local luacache = (_G.__luacache or {}).cache
-        if luacache then
-          luacache[mod.module] = nil
+    util.autocmd({
+      "WATCH_LUA_FILE BufWritePost",
+      module.name,
+      run = function()
+        for _, mod in ipairs(modules) do
+          package.loaded[mod.module] = nil
+          -- selene: allow(global_usage)
+          local luacache = (_G.__luacache or {}).cache
+          if luacache then
+            luacache[mod.module] = nil
+          end
+          require(mod.module)
         end
-        require(mod.module)
-      end
 
-      local msg = table.concat(names, "\n")
-      vim.notify(msg, vim.lsp.log_levels.INFO, {
-        title   = 'Reloaded',
-        timeout = 1000,
-      })
-    end, docs=string.format('watching %s', module.name)}
+        local msg = table.concat(names, "\n")
+        vim.notify(msg, vim.lsp.log_levels.INFO, {
+          title = "Reloaded",
+          timeout = 1000,
+        })
+      end,
+      docs = string.format("watching %s", module.name),
+    })
   end
 
   local msg = table.concat(names, "\n")
   vim.notify(msg, vim.lsp.log_levels.INFO, {
-    title   = 'Watching Changes',
+    title = "Watching Changes",
     timeout = 2000,
   })
 end
 
 util.command("WatchLuaFileChanges", function(arg)
-  local filename = vim.fn.expand('%:p')
+  local filename = vim.fn.expand("%:p")
   local files = {}
   if arg and arg.args ~= "" then
-    files = vim.split(arg.args, ' ') or {}
+    files = vim.split(arg.args, " ") or {}
   end
   table.insert(files, filename)
   M.watch_file_changes(files)
-end, {nargs='*', complete='file'})
+end, { nargs = "*", complete = "file" })
 
 util.command("CC", function()
   for _, win in ipairs(vim.api.nvim_list_wins()) do
@@ -93,74 +106,82 @@ util.command("CC", function()
 end)
 
 util.command("FoldComments", function()
-  vim.wo.foldexpr=[[getline(v:lnum)=~'^\s*//']]
-  vim.wo.foldmethod="expr"
+  vim.wo.foldexpr = [[getline(v:lnum)=~'^\s*//']]
+  vim.wo.foldmethod = "expr"
 end)
 
 util.buffer_command("Nowrap", function()
-  vim.bo.formatoptions = vim.bo.formatoptions:gsub('t', '')
-  vim.bo.formatoptions = vim.bo.formatoptions:gsub('c', '')
+  vim.bo.formatoptions = vim.bo.formatoptions:gsub("t", "")
+  vim.bo.formatoptions = vim.bo.formatoptions:gsub("c", "")
 end)
 
-util.command('ToggleRelativeNumbers', function()
+util.command("ToggleRelativeNumbers", function()
   vim.opt.relativenumber = vim.g.disable_relative_numbers or false
   vim.opt.number = true
   vim.g.disable_relative_numbers = not vim.g.disable_relative_numbers
 end)
 
 util.command("InstallDependencies", function()
-  local commands = _t{
-    golangci   = _t{"go", "install", "github.com/golangci/golangci-lint/cmd/golangci-lint@v1.43.0"},
-    gojq       = _t{"go", "install", "github.com/itchyny/gojq/cmd/gojq@latest"},
-    fixjson    = _t{"npm", "-g", "install", "--prefix", "~/.node_modules", "fixjson@latest"},
-    prettier   = _t{"npm", "-g", "install", "--prefix", "~/.node_modules", "prettier@latest"},
-    neovim     = _t{"npm", "-g", "install", "--prefix", "~/.node_modules", "neovim@latest"},
-    selene     = _t{'cargo', 'install', 'selene'},
-  }
+  local commands = _t({
+    golangci = _t({
+      "go",
+      "install",
+      "github.com/golangci/golangci-lint/cmd/golangci-lint@v1.43.0",
+    }),
+    gojq = _t({ "go", "install", "github.com/itchyny/gojq/cmd/gojq@latest" }),
+    fixjson = _t({ "npm", "-g", "install", "--prefix", "~/.node_modules", "fixjson@latest" }),
+    prettier = _t({ "npm", "-g", "install", "--prefix", "~/.node_modules", "prettier@latest" }),
+    neovim = _t({ "npm", "-g", "install", "--prefix", "~/.node_modules", "neovim@latest" }),
+    selene = _t({ "cargo", "install", "selene" }),
+    stylua = _t({ "cargo", "install", "stylua" }),
+  })
 
   local total = commands:map_length()
   local count = 0
 
-  local job = require('plenary.job')
+  local job = require("plenary.job")
   for name, spec in pairs(commands) do
-    job:new({
-      command = spec[1],
-      args = spec:slice(2, #spec),
-      on_exit = function(j, exit_code)
-        local res = table.concat(j:result(), "\n")
-        local type = vim.lsp.log_levels.INFO
-        local timeout = 2000
+    job
+      :new({
+        command = spec[1],
+        args = spec:slice(2, #spec),
+        on_exit = function(j, exit_code)
+          local res = table.concat(j:result(), "\n")
+          local type = vim.lsp.log_levels.INFO
+          local timeout = 2000
 
-        if exit_code ~=0 then
-          type = vim.lsp.log_levels.ERROR
-          res = table.concat(j:stderr_result(), "\n")
-          timeout = 10000
-        end
+          if exit_code ~= 0 then
+            type = vim.lsp.log_levels.ERROR
+            res = table.concat(j:stderr_result(), "\n")
+            timeout = 10000
+          end
 
-        vim.notify(res, type, {
-          title = name:title_case(),
-          timeout = timeout,
-        })
-
-        count = count + 1
-        if count == total then
-          local str =  "yay -S ripgrep bat ccls words-insane ctags python-pip the_silver_searcher && snap install diagon"
-          vim.schedule(function()
-            vim.fn.setreg('+', str)
-          end)
-          local data = table.concat({
-            "Please run:",
-            "    " .. str,
-            "",
-            "The command has been yanked to your clickboard!",
-          }, "\n")
-          vim.notify(data, vim.lsp.log_levels.INFO, {
-            title = "All done!",
-            timeout = 3000,
+          vim.notify(res, type, {
+            title = name:title_case(),
+            timeout = timeout,
           })
-        end
-      end,
-    }):start()
+
+          count = count + 1
+          if count == total then
+            local str =
+              "yay -S ripgrep bat words-insane ctags python-pip the_silver_searcher && snap install diagon"
+            vim.schedule(function()
+              vim.fn.setreg("+", str)
+            end)
+            local data = table.concat({
+              "Please run:",
+              "    " .. str,
+              "",
+              "The command has been yanked to your clickboard!",
+            }, "\n")
+            vim.notify(data, vim.lsp.log_levels.INFO, {
+              title = "All done!",
+              timeout = 3000,
+            })
+          end
+        end,
+      })
+      :start()
   end
 end)
 
--- lua/settings/null_ls.lua
+++ lua/settings/null_ls.lua
@@ -1,24 +1,37 @@
-local util = require('util')
-local null_ls = require('null-ls')
+local util = require("util")
+local null_ls = require("null-ls")
+
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.fixjson,
     null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.stylua.with({
+      extra_args = { "--indent-type=Spaces", "--indent-width=2", "--column-width=100" },
+    }),
     null_ls.builtins.diagnostics.golangci_lint,
     null_ls.builtins.diagnostics.selene,
   },
   on_attach = function(client)
     if client.resolved_capabilities.document_formatting then
-      util.autocmd{'BufWritePre', run=function()
-        vim.lsp.buf.formatting_sync()
-      end, buffer=true}
+      util.autocmd({
+        "BufWritePre",
+        run = function()
+          vim.lsp.buf.formatting_sync()
+        end,
+        buffer = true,
+      })
     end
     if client.resolved_capabilities.document_range_formatting then
-      util.buffer_command('Format', function(args)
-        require("settings.lsp.util").format_command(args.range ~= 0, args.line1, args.line2, args.bang)
-      end, {range=true})
-      util.vnoremap{"gq", ':Format<CR>', buffer=true, silent=true, desc='format range'}
-      vim.bo.formatexpr = 'v:lua.vim.lsp.formatexpr()'
+      util.buffer_command("Format", function(args)
+        require("settings.lsp.util").format_command(
+          args.range ~= 0,
+          args.line1,
+          args.line2,
+          args.bang
+        )
+      end, { range = true })
+      util.vnoremap({ "gq", ":Format<CR>", buffer = true, silent = true, desc = "format range" })
+      vim.bo.formatexpr = "v:lua.vim.lsp.formatexpr()"
     end
   end,
 })
