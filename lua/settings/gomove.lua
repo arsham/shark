7a057e3afe235ca5f93de2eb5cabe7931fc1247c
Arsham Shirvani <arshamshirvani@gmail.com>
2022-01-09 20:31:22 +0000

    ref: gomove new config

--- lua/settings/gomove.lua
+++ lua/settings/gomove.lua
@@ -1,6 +1,6 @@
 require("gomove").setup({
   map_defaults = true,
-  reindent_mode = "vim-move",
+  reindent = true,
   move_past_line = true,
   ignore_indent_lh_dup = true,
   move_past_end_col = true,
