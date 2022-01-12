b207b824d253411848f452727e884d8c68c11cf9
Arsham Shirvani <arshamshirvani@gmail.com>
2022-01-12 07:06:09 +0000

    feat(sqls): close key binding for the output

--- lua/settings/sqls.lua
+++ lua/settings/sqls.lua
@@ -1,3 +1,6 @@
+local quick = require("arshlib.quick")
+local nvim = require("nvim")
+
 vim.keymap.set(
   "n",
   "<C-Space>",
@@ -10,3 +13,16 @@ vim.keymap.set(
   ":SqlsExecuteQuery<CR>",
   { noremap = true, buffer = true, silent = true }
 )
+
+quick.augroup({
+  "SQLS_OUTPUT",
+  {
+    {
+      "FileType",
+      "sqls_output",
+      function()
+        vim.keymap.set("n", "q", nvim.ex.close, { noremap = true })
+      end,
+    },
+  },
+})
