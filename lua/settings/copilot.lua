01cf3f8a29c322b18916efa73ac1cf7f8b8f33ed
Arsham Shirvani <arshamshirvani@gmail.com>
2022-01-21 07:21:07 +0000

    ref(copilot): disable mapping when disabled

--- lua/settings/copilot.lua
+++ lua/settings/copilot.lua
@@ -1,17 +1,20 @@
 local nvim = require("nvim")
 
 -- stylua: ignore start
-vim.keymap.set("i", "<C-y>", [[copilot#Accept("\<CR>")]],
-  { noremap = true, silent = true, expr = true, script = true, desc = "copilot accept suggestion" }
-)
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
-vim.keymap.set("n", "<leader>ce", ":Copilot enable<cr>",
-  { noremap = true, silent = true, desc = "enable copilot" }
-)
-vim.keymap.set("n", "<leader>cd", ":Copilot disable<cr>",
-  { noremap = true, silent = true, desc = "disable copilot" }
-)
+
+vim.keymap.set("n", "<leader>ce", function()
+  nvim.ex.Copilot("enable")
+  vim.keymap.set("i", "<C-y>", [[copilot#Accept("\<CR>")]],
+  { noremap = true, silent = true, expr = true, script = true, desc = "copilot accept suggestion" })
+end, { noremap = true, silent = true, desc = "enable copilot" })
+
+vim.keymap.set("n", "<leader>cd", function()
+  nvim.ex.Copilot("disable")
+  vim.keymap.del("i", "<C-y>")
+end, { noremap = true, silent = true, desc = "disable copilot" })
 -- stylua: ignore end
+
 -- disabled by default
 nvim.ex.Copilot("disable")
