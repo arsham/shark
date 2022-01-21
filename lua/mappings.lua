d9a40a1782c6dd7a9071930b445b08a60f84537b
Arsham Shirvani <arshamshirvani@gmail.com>
2022-01-21 07:23:30 +0000

    style(mappings): wrapping changes

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,6 +1,5 @@
 local quick = require("arshlib.quick")
 
---vim.v.count
 --vim.api.nvim_put(t, 'l', true, false)
 
 vim.g.mapleader = " "
@@ -16,7 +15,8 @@ vim.keymap.set("i", "<Down>", "<Nop>", { noremap = true, desc = "disabling arrow
 vim.keymap.set("i", "<Left>", "<Nop>", { noremap = true, desc = "disabling arrows" })
 vim.keymap.set("i", "<Right>", "<Nop>", { noremap = true, desc = "disabling arrows" })
 -- }}}
--- Moving aroung {{{
+
+-- Moving around {{{
 -- stylua: ignore start
 vim.keymap.set("i", "<A-j>", [[<Esc>:<c-u>execute 'm +'. v:count1<cr>==gi]], { noremap = true, silent = true, desc = "move lines down" })
 vim.keymap.set("i", "<A-k>", [[<Esc>:<c-u>execute 'm -1-'. v:count1<cr>==gi]], { noremap = true, silent = true, desc = "move lines up" })
@@ -25,34 +25,21 @@ vim.keymap.set("x", "<", "<gv",{noremap=true, desc = "Keep the visually selected
 vim.keymap.set("x", ">", ">gv",{noremap=true, desc = "Keep the visually selected area when indenting" })
 
 vim.keymap.set("n", "g=", "gg=Gg``",{noremap=true, desc = "Re-indent the whole buffer" })
--- stylua: ignore end
 vim.keymap.set("n", "<C-e>", "2<C-e>", { noremap = true })
 vim.keymap.set("n", "<C-y>", "2<C-y>", { noremap = true })
 --}}}
 
 -- Resizing windows {{{
-vim.keymap.set(
-  "n",
-  "<M-Left>",
-  ":vert resize -2<CR>",
+vim.keymap.set("n", "<M-Left>", ":vert resize -2<CR>",
   { noremap = true, silent = true, desc = "decreases vertical size" }
 )
-vim.keymap.set(
-  "n",
-  "<M-Right>",
-  ":vert resize +2<CR>",
+vim.keymap.set("n", "<M-Right>", ":vert resize +2<CR>",
   { noremap = true, silent = true, desc = "increase vertical size" }
 )
-vim.keymap.set(
-  "n",
-  "<M-Up>",
-  ":resize +2<CR>",
+vim.keymap.set("n", "<M-Up>", ":resize +2<CR>",
   { noremap = true, silent = true, desc = "increase horizontal size" }
 )
-vim.keymap.set(
-  "n",
-  "<M-Down>",
-  ":resize -2<CR>",
+vim.keymap.set("n", "<M-Down>", ":resize -2<CR>",
   { noremap = true, silent = true, desc = "decreases horizontal size" }
 )
 --}}}
@@ -61,13 +48,7 @@ vim.keymap.set("n", "G", "Gzz", { noremap = true, desc = "Auto re-centre when mo
 vim.keymap.set("n", "g;", "m'g;zz", { noremap = true, desc = "Auto re-centre when moving around" })
 vim.keymap.set("n", "g,", "m'g,zz", { noremap = true, desc = "Auto re-centre when moving around" })
 
-vim.keymap.set(
-  "n",
-  "<Esc><Esc>",
-  ":noh<CR>",
-  { noremap = true, silent = true, desc = "Clear hlsearch" }
-)
--- stylua: ignore end
+vim.keymap.set("n", "<Esc><Esc>", ":noh<CR>", { noremap = true, silent = true, desc = "Clear hlsearch" })
 
 -- Yank related {{{
 vim.keymap.set("n", "<Leader>y", '"+y')
@@ -75,17 +56,11 @@ vim.keymap.set("x", "<Leader>y", '"+y')
 vim.keymap.set("n", "<Leader>p", '"+p')
 vim.keymap.set("n", "<Leader>P", '"+P')
 
-vim.keymap.set(
-  "v",
-  "p",
-  '"_dP',
+vim.keymap.set("v", "p", '"_dP',
   { noremap = true, desc = 'replace visually selected with the " contents' }
 )
 --}}}
-vim.keymap.set(
-  "n",
-  "<leader>gw",
-  ":silent lgrep <cword> % <CR>",
+vim.keymap.set("n", "<leader>gw", ":silent lgrep <cword> % <CR>",
   { noremap = true, silent = true, desc = "grep on local buffer" }
 )
 
@@ -103,32 +78,16 @@ vim.keymap.set("n", "<leader>sf", function()
     vim.wo.spell = spell
   end)
 end, { noremap = true, desc = "auto correct spelling and jump bak." }) --}}}
+
 -- Merge tool {{{
-vim.keymap.set(
-  "n",
-  "<leader>1",
-  ":diffget LOCAL<CR>",
-  { noremap = true, desc = "mergetool mapping" }
-)
-vim.keymap.set(
-  "n",
-  "<leader>2",
-  ":diffget BASE<CR>",
-  { noremap = true, desc = "mergetool mapping" }
-)
-vim.keymap.set(
-  "n",
-  "<leader>3",
-  ":diffget REMOTE<CR>",
-  { noremap = true, desc = "mergetool mapping" }
-)
+vim.keymap.set("n", "<leader>1", ":diffget LOCAL<CR>",  { noremap = true, desc = "mergetool mapping" })
+vim.keymap.set("n", "<leader>2", ":diffget BASE<CR>",   { noremap = true, desc = "mergetool mapping" })
+vim.keymap.set("n", "<leader>3", ":diffget REMOTE<CR>", { noremap = true, desc = "mergetool mapping" })
 --}}}
+
 vim.keymap.set("n", "<leader>jq", ":%!gojq '.'<CR>")
 
-vim.keymap.set(
-  "n",
-  "<leader>hh",
-  ":h <CR>",
+vim.keymap.set("n", "<leader>hh", ":h <CR>",
   { noremap = true, desc = "Show help for work under the cursor" }
 )
 -- stylua: ignore end
@@ -173,4 +132,5 @@ vim.keymap.set("n", "<leader>zs", function()
   vim.opt_local.foldmethod = "syntax"
 end, { noremap = true, silent = true, desc = "set local foldmethod to syntax" })
 --}}}
--- vim: foldmethod=marker
+
+-- vim: fdm=marker fdl=0
