0971a52f8afc269b54411b069ba74639c212a670
Arsham Shirvani <arshamshirvani@gmail.com>
2022-02-13 14:18:04 +0000

    fix(sqls): keybindings

--- lua/settings/sqls.lua
+++ lua/settings/sqls.lua
@@ -1,28 +1,20 @@
 local quick = require("arshlib.quick")
 local nvim = require("nvim")
 
-vim.keymap.set(
-  "n",
-  "<C-Space>",
-  ":SqlsExecuteQuery<CR>",
-  { noremap = true, buffer = true, silent = true }
-)
-vim.keymap.set(
-  "v",
-  "<C-Space>",
-  ":SqlsExecuteQuery<CR>",
-  { noremap = true, buffer = true, silent = true }
-)
+-- stylua: ignore start
 
-quick.augroup({
-  "SQLS_OUTPUT",
-  {
-    {
-      "FileType",
-      "sqls_output",
-      function()
-        vim.keymap.set("n", "q", nvim.ex.close, { noremap = true, buffer = true })
-      end,
-    },
-  },
-})
+quick.augroup({"SQLS_SETTINGS", {
+  {"FileType", "sqls_output", function()
+      vim.keymap.set("n", "q", nvim.ex.close, { noremap = true, buffer = true })
+  end},
+  {"FileType", "sql", function()
+    vim.keymap.set("n", "<C-Space>", ":SqlsExecuteQuery<CR>",
+      { noremap = true, buffer = true, silent = true }
+    )
+    vim.keymap.set("v", "<C-Space>", ":SqlsExecuteQuery<CR>",
+      { noremap = true, buffer = true, silent = true }
+    )
+  end}
+}})
+
+-- stylua: ignore end
