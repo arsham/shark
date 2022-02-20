4356d418c49b67a3de3f9569a45f163299cca1f7
Arsham Shirvani <arshamshirvani@gmail.com>
2022-02-20 11:56:11 +0000

    ref(plugins): convert dial.nvim configuration
    A couple of missing config are converted from the author's code from the
    old implementation.

--- lua/settings/dial-nvim.lua
+++ lua/settings/dial-nvim.lua
@@ -1,52 +1,104 @@
-local dial = require("dial")
+local augend = require("dial.augend")
+local config = require("dial.config")
+local common = require("dial.augend.common")
 
-dial.augends["custom#boolean"] = dial.common.enum_cyclic({
-  name = "boolean",
-  strlist = { "true", "false" },
-})
+---Returns a list of all characters in the given string.
+-- @param str The string to get the characters from.
+-- @return table
+local function generate_list(str)
+  local ret = {}
+  for i = 1, #str, 1 do
+    table.insert(ret, str:sub(i, i))
+  end
+  return ret
+end
 
-dial.augends["custom#weekday"] = dial.common.enum_cyclic({
-  name = "weekday",
-  strlist = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" },
-})
+---Increases the given number by one.
+-- @param text string The number to increase.
+-- @param addend string The number to add to the given number.
+-- @param prefix string|nil prefix of formatting the number
+-- @return table
+local function increase_decimal(text, addend, prefix)
+  local len = #text
+  local n = tonumber(text)
+  n = n + addend
+  if n < 0 then
+    n = 0
+  end
+  if n > (10 ^ len) - 1 then
+    n = (10 ^ len) - 1
+  end
+  prefix = prefix or ""
+  local format = "%" .. prefix .. "d"
+  text = format:format(n)
+  return { text = text, cursor = len }
+end
 
-dial.augends["custom#go#integer"] = dial.common.enum_cyclic({
-  name = "integet",
-  strlist = { "int", "int64", "int32" },
-})
+config.augends:register_group({
+  default = {
+    augend.constant.alias.bool,
+    augend.date.alias["%-m/%-d"],
+    augend.date.alias["%H:%M"],
+    augend.date.alias["%H:%M:%S"],
+    augend.date.alias["%Y-%m-%d"],
+    augend.date.alias["%Y/%m/%d"],
+    augend.date.alias["%m/%d"],
+    augend.hexcolor.new({}),
+    augend.integer.alias.binary,
+    augend.integer.alias.decimal,
+    augend.integer.alias.hex,
+    augend.integer.alias.octal,
+    augend.semver.alias.semver,
 
-dial.augends["custom#go#float"] = dial.common.enum_cyclic({
-  name = "float",
-  strlist = { "float64", "float32" },
-})
+    augend.constant.new({ elements = generate_list("abcdefghijklmnopqrstuvwxyz"), cyclic = true }),
+    augend.constant.new({ elements = generate_list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), cyclic = true }),
+    augend.constant.new({
+      elements = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" },
+    }),
+    augend.constant.new({ elements = { "int", "int64", "int32" } }),
+    augend.constant.new({ elements = { "float64", "float32" } }),
+    -- copied from an old commit from the author of the plugin.
+    augend.user.new({
+      desc = "fixed-digit decimal natural number (e.g. 00, 01, 02, ..., 97, 98, 99)",
+      find = common.find_pattern("%d+"),
+      add = function(text, addend)
+        return increase_decimal(text, addend, "0")
+      end,
+    }),
 
-vim.list_extend(dial.config.searchlist.normal, {
-  "char#alph#capital#str",
-  "char#alph#capital#word",
-  "char#alph#small#str",
-  "char#alph#small#word",
-  "color#hex",
-  "date#[%H:%M]",
-  "date#[%H:%M:%S]",
-  "date#[%ja]",
-  "date#[%jA]",
-  "date#[%-m/%-d]",
-  "date#[%m/%d]",
-  "date#[%Y-%m-%d]",
-  "date#[%Y/%m/%d]",
-  "markup#markdown#header",
-  "number#binary",
-  "number#decimal",
-  "number#decimal#fixed#space",
-  "number#decimal#fixed#zero",
-  "number#decimal#int",
-  "number#hex",
-  "number#octal",
-  -- custom augends
-  "custom#boolean",
-  "custom#weekday",
-  "custom#go#integer",
-  "custom#go#float",
+    augend.user.new({
+      desc = "fixed-digit decimal natural number (e.g. ␣0, ␣1, ␣2, ..., 97, 98, 99)",
+      find = common.find_pattern(" *%d+"),
+      add = function(text, addend)
+        return increase_decimal(text, addend)
+      end,
+    }),
+
+    augend.user.new({
+      desc = "Markdown Header (# Title)",
+
+      find = function(line)
+        local from, to = line:find("^#+")
+        if from == nil or to > 7 then
+          return nil
+        end
+        return { from = from, to = to }
+      end,
+
+      add = function(text, addend)
+        local n = #text
+        n = n + addend
+        if n < 1 then
+          n = 1
+        end
+        if n > 6 then
+          n = 6
+        end
+        text = ("#"):rep(n)
+        return { text = text, cursor = 1 }
+      end,
+    }),
+  },
 })
 
 vim.keymap.set("n", "<C-a>", "<Plug>(dial-increment)", { silent = true })
