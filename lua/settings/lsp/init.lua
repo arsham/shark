9af250467b608966d7d8d18bd4782de05bc70a28
Arsham Shirvani <arshamshirvani@gmail.com>
2022-02-21 20:17:15 +0000

    ref(plugins): disable html formatting

--- lua/settings/lsp/init.lua
+++ lua/settings/lsp/init.lua
@@ -148,6 +148,17 @@ local servers = {
       return opts
     end,
   }, --}}}
+
+  html = {
+    update = function(on_attach, opts)
+      opts.on_attach = function(client, bufnr)
+        client.resolved_capabilities.document_formatting = false
+        client.resolved_capabilities.document_range_formatting = false
+        on_attach(client, bufnr)
+      end
+      return opts
+    end,
+  },
 }
 
 local lsp_util = require("settings.lsp.util")
