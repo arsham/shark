6ffe76a7d57974269a9f90b55e88426a1fbcfe1c
Arsham Shirvani <arshamshirvani@gmail.com>
2022-02-27 13:16:55 +0000

    fix(dial): key bindings

--- lua/settings/dial-nvim.lua
+++ lua/settings/dial-nvim.lua
@@ -120,9 +120,9 @@ config.augends:register_group({
   },
 })
 
-vim.keymap.set("n", "<C-a>", "<Plug>(dial-increment)", { silent = true })
-vim.keymap.set("n", "<C-x>", "<Plug>(dial-decrement)", { silent = true })
-vim.keymap.set("v", "<C-a>", "<Plug>(dial-increment)", { silent = true })
-vim.keymap.set("v", "<C-x>", "<Plug>(dial-decrement)", { silent = true })
-vim.keymap.set("v", "g<C-a>", "<Plug>(dial-increment-additional)", { silent = true })
-vim.keymap.set("v", "g<C-x>", "<Plug>(dial-decrement-additional)", { silent = true })
+vim.keymap.set("n", "<C-a>", require("dial.map").inc_normal(), { noremap = true })
+vim.keymap.set("n", "<C-x>", require("dial.map").dec_normal(), { noremap = true })
+vim.keymap.set("v", "<C-a>", require("dial.map").inc_visual(), { noremap = true })
+vim.keymap.set("v", "<C-x>", require("dial.map").dec_visual(), { noremap = true })
+vim.keymap.set("v", "g<C-a>", require("dial.map").inc_gvisual(), { noremap = true })
+vim.keymap.set("v", "g<C-x>", require("dial.map").dec_gvisual(), { noremap = true })
