dd29be5aad78b04be489385b1153ae41aeba2532
Arsham Shirvani <arshamshirvani@gmail.com>
2022-04-20 22:15:05 +0100

    ref(plugins): fix cmp settings

--- lua/settings/cmp.lua
+++ lua/settings/cmp.lua
@@ -42,19 +42,37 @@ local function has_words_before()
     and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
+local function tab_function(fallback)
+  if ls.expand_or_locally_jumpable() then
+    ls.expand_or_jump()
+  elseif has_words_before() then
+    cmp.complete()
+  else
+    fallback()
+  end
+end
+
+local function shift_tab_function(fallback)
+  if ls.jumpable(-1) then
+    ls.jump(-1)
+  else
+    fallback()
+  end
+end
+
 cmp.setup({
   snippet = {
     expand = function(args)
-      require("luasnip").lsp_expand(args.body)
+      ls.lsp_expand(args.body)
     end,
   },
 
   preselect = cmp.PreselectMode.None,
 
-  mapping = { --{{{
-    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+  mapping = cmp.mapping.preset.insert({ --{{{
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-y>"] = cmp.config.disable,
     ["<C-e>"] = cmp.mapping({
       i = cmp.mapping.abort(),
@@ -73,24 +91,16 @@ cmp.setup({
     }),
 
     -- only use < tab >/<s-tab> for switching between placeholders.
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if ls.expand_or_locally_jumpable() then
-        ls.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
+    ["<Tab>"] = cmp.mapping({
+      i = tab_function,
+      s = tab_function,
+    }),
 
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if ls.jumpable(-1) then
-        ls.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-  }, --}}}
+    ["<S-Tab>"] = cmp.mapping({
+      i = shift_tab_function,
+      s = shift_tab_function,
+    }),
+  }), --}}}
 
   sources = cmp.config.sources({ --{{{
     { name = "nvim_lsp", priority = 80 },
@@ -145,8 +155,9 @@ cmp.setup({
     end,
   }, --}}}
 
-  documentation = {
-    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
   },
 
   sorting = { --{{{
@@ -165,11 +176,5 @@ cmp.setup({
     },
   }, --}}}
 })
-
-cmp.setup.cmdline("/", { --{{{
-  sources = {
-    { name = "buffer" },
-  },
-}) --}}}
 
 -- vim: fdm=marker fdl=0
