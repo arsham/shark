ec0968439d6408ca4872279596996f855d6b24d5
Arsham Shirvani <arshamshirvani@gmail.com>
2022-04-20 22:26:16 +0100

    ref(plugins): set fzf-lua as default fzfmania ui

--- lua/settings/fzfmania.lua
+++ lua/settings/fzfmania.lua
@@ -2,21 +2,24 @@ local command = require("arshlib.quick").command
 
 table.insert(vim.opt.rtp, "~/.fzf")
 
-require("fzfmania").config({})
+require("fzfmania").config({
+  frontend = "fzf-lua",
+})
 
 command("Notes", "call fzf#vim#files('~/Dropbox/Notes', <bang>0)", { bang = true })
 command("Dotfiles", "call fzf#vim#files('~/dotfiles/', <bang>0)", { bang = true })
 
 vim.keymap.set("n", "<leader>fl", function()
-  require("arshlib.util").user_input({
+  vim.ui.input({
     prompt = "Term: ",
-    on_submit = function(term)
+  }, function(term)
+    if term then
       vim.schedule(function()
         local preview = vim.fn["fzf#vim#with_preview"]()
         vim.fn["fzf#vim#locate"](term, preview)
       end)
-    end,
-  })
-end, { noremap = true, silent = true, desc = "run locate" })
+    end
+  end)
+end, { silent = true, desc = "run locate" })
 
 -- vim: foldmethod=marker foldlevel=0
