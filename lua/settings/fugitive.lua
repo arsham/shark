e49cda3e0c366556e4e2eb34272d7b771cbc4cf7
Arsham Shirvani <arshamshirvani@gmail.com>
2022-05-04 20:28:42 +0100

    feat(plugins): add Glg to fugitive for log graph

--- lua/settings/fugitive.lua
+++ lua/settings/fugitive.lua
@@ -1,1 +1,8 @@
 vim.keymap.set("n", "<leader>gg", ":Git<cr>", { silent = true, desc = "open fugitive" })
+local opts = {
+  force = true,
+  nargs = "*",
+  desc = "Git log graph",
+}
+local command = "Git! lg <args>"
+vim.api.nvim_create_user_command("Glg", command, opts)
