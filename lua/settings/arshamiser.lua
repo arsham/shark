6ced58de1b332a5ad20ec880722d1ca732fcf54e
Arsham Shirvani <arshamshirvani@gmail.com>
2022-06-27 23:03:44 +0100

    feat(arshamiser): add tabline settings

--- lua/settings/arshamiser.lua
+++ lua/settings/arshamiser.lua
@@ -9,5 +9,7 @@ vim.api.nvim_create_autocmd("UIEnter", {
     -- selene: allow(global_usage)
     _G.custom_foldtext = require("arshamiser.folding").foldtext
     vim.opt.foldtext = "v:lua.custom_foldtext()"
+
+    vim.api.nvim_set_option("tabline", [[%{%v:lua.require("arshamiser.tabline").draw()%}]])
   end,
 })
