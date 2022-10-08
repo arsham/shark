1ef1102611df5b9e4cbab5bfaae69d0d78e19a5f
Arsham Shirvani <arshamshirvani@gmail.com>
2022-10-09 00:03:07 +0100

    ref(go): set conceal and fold levels

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -2,8 +2,8 @@ vim.bo.tabstop = 4
 vim.bo.shiftwidth = 4
 vim.bo.softtabstop = 4
 vim.opt_local.formatoptions:remove({ "t" })
-
-vim.bo.comments = "s1:/*,mb:*,ex:*/,://"
 vim.bo.expandtab = false
 
 vim.api.nvim_command("compiler go")
+vim.opt_local.conceallevel = 2
+vim.opt_local.foldlevel = 3
