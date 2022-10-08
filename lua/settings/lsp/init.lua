df8945c0e4300d044b1b8e8bdc86303c6497edf9
Arsham Shirvani <arshamshirvani@gmail.com>
2022-10-08 23:41:26 +0100

    ref(lsp): add/update various servers and config

--- lua/settings/lsp/init.lua
+++ lua/settings/lsp/init.lua
@@ -1,6 +1,7 @@
 -- vim.lsp.set_log_level("debug")
 
-local util = require("util")
+require("arshlib.tables")
+local quick = require("arshlib.quick")
 
 local signs = {
   Error = "🔥",
@@ -21,9 +22,7 @@ for type in pairs(signs) do --{{{
 end
 --}}}
 vim.diagnostic.config({ --{{{
-  virtual_text = {
-    prefix = "👈",
-  },
+  virtual_text = false,
   signs = true,
   underline = true,
   update_in_insert = false,
@@ -45,25 +44,33 @@ local servers = {
         gopls = {
           analyses = {
             unusedparams = true,
+            nillness = true,
+            unusedwrites = true,
+            useany = true,
+            unusedvariable = true,
           },
           completeUnimported = true,
           staticcheck = true,
           buildFlags = { "-tags=integration,e2e" },
-          hoverKind = "FullDocumentation",
-          linkTarget = "pkg.go.dev",
           linksInHover = true,
-          experimentalWorkspaceModule = true,
-          experimentalPostfixCompletions = true,
           codelenses = {
             generate = true,
             gc_details = true,
             test = true,
             tidy = true,
+            run_vulncheck_exp = true,
+            upgrade_dependency = true,
           },
           usePlaceholders = true,
-
+          directoryFilters = {
+            "-**/node_modules",
+            "-/tmp",
+          },
           completionDocumentation = true,
           deepCompletion = true,
+          semanticTokens = true,
+          completionBudget = "0",
+          verboseOutput = false, -- useful for debugging when true.
         },
       },
     },
@@ -73,17 +80,36 @@ local servers = {
     opts = {},
     update = function(on_attach, opts)
       opts = require("lua-dev").setup({
+        library = {
+          plugins = { "arshlib.nvim", "plenary.nvim" },
+        },
+        runtime_path = true,
         lspconfig = {
           on_attach = on_attach,
           capabilities = opts.capabilities,
           settings = {
             Lua = {
+              runtime = {
+                version = "LuaJIT",
+                path = {
+                  "?/init.lua",
+                  "?.lua",
+                },
+              },
               diagnostics = {
-                globals = { "vim", "use", "require", "rocks", "use_rocks" },
+                globals = {
+                  "vim",
+                  "use",
+                  "require",
+                  "rocks",
+                  "use_rocks",
+                },
               },
               workspace = {
-                maxPreload = 100000,
-                preloadFileSize = 50000,
+                ignoreDir = "tmp/",
+                useGitIgnore = false,
+                maxPreload = 100000000,
+                preloadFileSize = 500000,
               },
             },
           },
@@ -137,14 +163,40 @@ local servers = {
   sqls = { --{{{
     opts = {},
     update = function(on_attach, opts)
-      -- neovim's LSP client does not currently support dynamic capabilities registration.
-      -- sqls has a bad formatting.
+      local function code_action(range_given, line1, line2)
+        if range_given then
+          vim.lsp.buf.code_action({
+            range = {
+              start = { line1, 0 },
+              ["end"] = { line2, 99999999 },
+            },
+          })
+        else
+          vim.lsp.buf.code_action()
+        end
+      end
+
+      -- neovim's LSP client does not currently support dynamic capabilities
+      -- registration.
       opts.on_attach = function(client, bufnr)
+        -- sqls has a bad formatting.
         client.server_capabilities.documentFormattingProvider = false
         client.server_capabilities.executeCommandProvider = true
-        client.commands = require("sqls").commands
-        require("sqls").setup({ picker = "fzf" })
+        client.server_capabilities.codeActionProvider = { resolveProvider = false }
+        local sqls = require("sqls")
+        sqls.on_attach(client, bufnr)
+        client.commands = sqls.commands
         on_attach(client, bufnr)
+
+        -- the picker is not compatible with fzf-lua.
+        quick.buffer_command("CodeAction", function(args)
+          code_action(args.range ~= 0, args.line1, args.line2)
+        end, { range = true })
+        local o = { desc = "SQL code action", buffer = true }
+        vim.keymap.set("n", "<localleader>ca", vim.lsp.buf.code_action, o)
+        vim.keymap.set("v", ":'<,'>CodeAction<CR>", "Code action", { buffer = true, silent = true })
+        vim.keymap.set("n", "<C-Space>", ":SqlsExecuteQuery<CR>", { buffer = true, silent = true })
+        vim.keymap.set("v", "<C-Space>", ":SqlsExecuteQuery<CR>", { buffer = true, silent = true })
       end
       return opts
     end,
@@ -166,7 +218,12 @@ local servers = {
   jedi_language_server = {},
   tsserver = {},
   vimls = {},
-  clangd = {
+  marksman = {},
+  bufls = {},
+  clangd = { --{{{
+    opts = {
+      filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
+    },
     update = function(on_attach, opts)
       opts.on_attach = function(client, bufnr)
         if vim.fn.findfile("uncrustify.cfg", ".;") ~= "" then
@@ -177,15 +234,16 @@ local servers = {
       end
       return opts
     end,
-  },
-  rust_analyzer = {
+  }, --}}}
+
+  rust_analyzer = { --{{{
     update = function(on_attach, opts)
       opts.on_attach = function(client, bufnr)
         on_attach(client, bufnr)
       end
       return opts
     end,
-  },
+  }, --}}}
 }
 
 
@@ -193,17 +251,33 @@ local on_attach = require("settings.lsp.on_attach").on_attach
 
 local null_ls = require("null-ls") -- NULL LS Setup {{{
 null_ls.setup({
+  debug = false,
   sources = {
     null_ls.builtins.formatting.fixjson,
     null_ls.builtins.formatting.prettier.with({
       disabled_filetypes = { "html" },
+      extra_args = function(params)
+        return params.options
+          and params.options.tabSize
+          and {
+            "--tab-width",
+            params.options.tabSize,
+          }
+      end,
     }),
     null_ls.builtins.formatting.stylua.with({
       extra_args = { "--indent-type=Spaces", "--indent-width=2", "--column-width=100" },
     }),
     null_ls.builtins.diagnostics.golangci_lint,
     null_ls.builtins.diagnostics.selene,
+    null_ls.builtins.diagnostics.hadolint,
+    null_ls.builtins.formatting.cbfmt.with({
+      extra_args = { "--config", vim.fn.stdpath("config") .. "/scripts/cbfmt.toml" },
+    }),
+    null_ls.builtins.formatting.autopep8,
     null_ls.builtins.diagnostics.buf,
+    null_ls.builtins.formatting.buf,
+    locallinters.api_linter,
     null_ls.builtins.formatting.uncrustify.with({
       extra_args = function()
         return {
@@ -236,5 +310,11 @@ for name, server in pairs(servers) do
   end
   lspconfig[name].setup(opts)
 end --}}}
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+  stylize_markdown = true,
+  syntax = "lsp_markdown",
+  border = "single",
+})
 
 -- vim: fdm=marker fdl=0
