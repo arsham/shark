98b120242efba0404d61605563b1ff7be50bc4b3
Arsham Shirvani <arshamshirvani@gmail.com>
2022-10-22 11:54:33 +0100

    fix(lsp): replace update_capabilities

--- lua/settings/lsp/init.lua
+++ lua/settings/lsp/init.lua
@@ -290,10 +290,13 @@ null_ls.setup({
 }) --}}}
 
 -- Enable (broadcasting) snippet capability for completion.
-local capabilities = require("cmp_nvim_lsp").update_capabilities( --{{{
-  vim.lsp.protocol.make_client_capabilities()
-)
-capabilities.textDocument.completion.completionItem.snippetSupport = true
+local capabilities = require("cmp_nvim_lsp").default_capabilities({
+  textDocument = {
+    completion = {
+      snippetSupport = true,
+    },
+  },
+}) --{{{
 --}}}
 
 local lspconfig = require("lspconfig") -- LSP Config Setup {{{
