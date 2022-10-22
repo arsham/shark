0e9018d05f40682c415ed471b032b60a78113b20
Arsham Shirvani <arshamshirvani@gmail.com>
2022-10-22 11:09:02 +0100

    fix(mappings): visual mode moving in cmdheight=0

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -22,14 +22,15 @@ vim.keymap.set("i", "<Right>", "<Nop>", o)
 -- }}}
 
 -- Moving around {{{
-vim.keymap.set("v", "<A-j>", [[:m '>+1<CR>gv=gv]], { silent = true, desc = "move lines down" })
-vim.keymap.set("v", "<A-k>", [[:m '<-2<CR>gv=gv]], { silent = true, desc = "move lines up" })
 local up = opts("move lines up")
 local down = opts("move lines down")
 vim.keymap.set("i", "<A-j>", [[<Esc>:<c-u>execute 'm +'. v:count1<cr>==gi]], down)
 vim.keymap.set("i", "<A-k>", [[<Esc>:<c-u>execute 'm -1-'. v:count1<cr>==gi]], up)
 vim.keymap.set("n", "<A-j>", [[:<c-u>execute 'm +'. v:count1<cr>==]], down)
 vim.keymap.set("n", "<A-k>", [[:<c-u>execute 'm -1-'. v:count1<cr>==]], up)
+-- double enter fixes the issues with cmdheight=0
+vim.keymap.set("v", "<A-j>", [[:m '>+1<CR><CR>gv=gv]], down)
+vim.keymap.set("v", "<A-k>", [[:m '<-2<CR><CR>gv=gv]], up)
 
 o = opts("keep the visually selected area when indenting")
 vim.keymap.set("x", "<", "<gv", o)
