b463443d142542c9db1b64d45ee7f0632df359fb
Arsham Shirvani <arshamshirvani@gmail.com>
2022-12-11 22:31:36 +0000

    ref(rust): force foldlevel to 3 for rust buffers

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -4,3 +4,7 @@ vim.bo.softtabstop = 4
 vim.opt_local.formatoptions:remove({ "t" })
 vim.bo.expandtab = true
 vim.wo.colorcolumn = "80,100,120"
+
+if not vim.opt_local.diff:get() then
+  vim.opt_local.foldlevel = 3
+end
