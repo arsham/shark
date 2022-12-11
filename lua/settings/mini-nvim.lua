4e52d3bee8acc2d4514dcb9e6f048781886e17ea
Arsham Shirvani <arshamshirvani@gmail.com>
2022-12-11 23:29:51 +0000

    ref(mini): add treesitter surround function

--- lua/settings/mini-nvim.lua
+++ lua/settings/mini-nvim.lua
@@ -3,7 +3,9 @@ quick.highlight("MiniTrailspace", { link = "ExtraWhitespace" })
 quick.highlight("MiniSurround", { link = "Substitute" })
 quick.highlight("MiniIndentscopeSymbol", { link = "VertSplit" })
 
-require("mini.surround").setup({
+local ts_input = require("mini.surround").gen_spec.input.treesitter
+local surround = require("mini.surround")
+surround.setup({
   n_lines = 40,
   highlight_duration = 1000,
 
@@ -15,6 +17,29 @@ require("mini.surround").setup({
     find_left = "yF",
     highlight = "yh",
     update_n_lines = "yn",
+
+    suffix_last = "l", -- Suffix to search with "prev" method
+    suffix_next = "n", -- Suffix to search with "next" method
+  },
+
+  custom_surroundings = {
+    -- Use tree-sitter to search for function call
+    f = {
+      input = ts_input({ outer = "@call.outer", inner = "@call.inner" }),
+    },
+    -- Use function to compute surrounding info
+    ["*"] = {
+      input = function()
+        local n_star = surround.user_input("Number of * to find: ")
+        local many_star = string.rep("%*", tonumber(n_star) or 1)
+        return { many_star .. "().-()" .. many_star }
+      end,
+      output = function()
+        local n_star = surround.user_input("Number of * to output: ")
+        local many_star = string.rep("*", tonumber(n_star) or 1)
+        return { left = many_star, right = many_star }
+      end,
+    },
   },
 })
 
