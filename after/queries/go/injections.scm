6b4f2eef41225bc1f4f6f7e6536f8c5196114300
Arsham Shirvani <arshamshirvani@gmail.com>
2022-12-11 22:33:02 +0000

    ref(go,queries): add set to sql matches

--- after/queries/go/injections.scm
+++ after/queries/go/injections.scm
@@ -4,4 +4,6 @@
    [
     (interpreted_string_literal)
     (raw_string_literal)] @sql
-      (#match? @sql "(SELECT|select|INSERT|insert|UPDATE|update|DELETE|delete).+(FROM|from|INTO|into|VALUES|values).*(WHERE|where|GROUP BY|group by)?"))))
+      (#match? @sql "(SELECT|select|INSERT|insert|UPDATE|update|DELETE|delete).+(FROM|from|INTO|into|VALUES|values|SET|set).*(WHERE|where|GROUP BY|group by)?"))))
+
+;; vim: formatoptions-=t
