f9bfb8c9a2da7c6484bd92c96c5875ca6c0bc06a
Arsham Shirvani <arshamshirvani@gmail.com>
2022-12-11 23:32:37 +0000

    feat(treesitter): add lua highlights/injections

---
+++ after/queries/lua/highlights.scm
@@ -0,0 +1,17 @@
+;; Add _ to constant
+((identifier) @constant
+ (#lua-match? @constant "^[_A-Z][A-Z_0-9]*$"))
+
+(
+  (identifier) @function
+  (#eq? @function "utils")
+  (#set! conceal "")
+)
+
+(
+  (dot_index_expression
+    table: (identifier) @keyword
+    (#eq? @keyword  "utils" )
+  )
+  (#set! conceal "U")
+)
