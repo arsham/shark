bac95d85f97c4de8364b04abf72df55790d36172
Arsham Shirvani <arshamshirvani@gmail.com>
2022-12-23 13:37:01 +0000

    ref(startuptime): move plugin spec to plugins module

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,14 +1,6 @@
 return {
-  -- Core/System utilities {{{
   "nvim-lua/plenary.nvim",
-
-  { -- Startup Time {{{
-    "tweekmonster/startuptime.vim",
-    cmd = { "StartupTime" },
-  }, -- }}}
-
   "samjwill/nvim-unception",
-  -- }}}
 }
 
 -- vim: fdm=marker fdl=1
