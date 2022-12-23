1de2994699cebc33efe3c2e639aff35016a7efdb
Arsham Shirvani <arshamshirvani@gmail.com>
2022-12-23 13:20:41 +0000

    ref(caser): move plugin spec to plugins module

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -29,21 +29,6 @@ return {
   -- }}}
 
   -- Editing {{{
-  { -- Caser {{{
-    -- MixedCase/PascalCase:   gsm/gsp
-    -- camelCase:              gsc
-    -- snake_case:             gs_
-    -- UPPER_CASE:             gsu/gsU
-    -- Title Case:             gst
-    -- Sentence case:          gss
-    -- space case:             gs<space>
-    -- dash-case/kebab-case:   gs-/gsk
-    -- Title-Dash/Title-Kebab: gsK
-    -- dot.case:               gs.
-    "arthurxavierx/vim-caser",
-    keys = { "gs" },
-  }, -- }}}
-
   { -- Yank Assasin {{{
     "svban/YankAssassin.vim",
     event = { "VeryLazy" },
