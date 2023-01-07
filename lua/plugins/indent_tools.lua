8d01c650c4e0fbe364c3e678eff30726eb5b0e23
Arsham Shirvani <arshamshirvani@gmail.com>
2023-01-07 23:01:21 +0000

    ref(indent_tools): streamline the spec

--- lua/plugins/indent_tools.lua
+++ lua/plugins/indent_tools.lua
@@ -1,9 +1,7 @@
 return {
   "arsham/indent-tools.nvim",
   dependencies = { "arsham/arshlib.nvim" },
-  config = function()
-    require("indent-tools").config({})
-  end,
   event = { "VeryLazy" },
   cond = require("util").full_start,
+  config = true,
 }
