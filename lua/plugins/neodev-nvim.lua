e7021b4ff102bdedf36fc5d9c444cf75357257ac
Arsham Shirvani <arshamshirvani@gmail.com>
2023-01-07 23:42:10 +0000

    ref(neodev): streamline the spec

--- lua/plugins/neodev-nvim.lua
+++ lua/plugins/neodev-nvim.lua
@@ -1,15 +1,13 @@
 return {
   "folke/neodev.nvim",
-  config = function()
-    require("neodev").setup({
-      library = {
-        plugins = { "arshlib.nvim", "plenary.nvim", "neotest" },
-        types = true,
-      },
-      runtime_path = true,
-    })
-  end,
   dependencies = { "neovim/nvim-lspconfig" },
   ft = { "lua" },
   cond = require("util").full_start_with_lsp,
+  config = {
+    library = {
+      plugins = { "arshlib.nvim", "plenary.nvim", "neotest" },
+      types = true,
+    },
+    runtime_path = true,
+  },
 }
