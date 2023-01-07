62b51dbb080c3438af633eb174b9774d4cabb601
Arsham Shirvani <arshamshirvani@gmail.com>
2023-01-07 23:00:05 +0000

    ref(go.nvim): streamline the spec

--- lua/plugins/go_nvim.lua
+++ lua/plugins/go_nvim.lua
@@ -1,14 +1,12 @@
 return {
   "ray-x/go.nvim",
-  config = function()
-    require("go").setup({
-      gofmt = "gofumpt",
-      lsp_codelens = false,
-      lsp_diag_hdlr = true,
-      textobjects = false,
-      run_in_floaterm = false,
-    })
-  end,
   ft = { "go" },
   cond = require("util").full_start_with_lsp,
+  config = {
+    gofmt = "gofumpt",
+    lsp_codelens = false,
+    lsp_diag_hdlr = true,
+    textobjects = false,
+    run_in_floaterm = false,
+  },
 }
