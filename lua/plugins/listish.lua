d062ca58c542834983d8b1899f44e64fca9fb9c4
Arsham Shirvani <arshamshirvani@gmail.com>
2023-01-07 23:20:41 +0000

    ref(listish): streamline the spec

--- lua/plugins/listish.lua
+++ lua/plugins/listish.lua
@@ -2,7 +2,5 @@ return {
   "arsham/listish.nvim",
   dependencies = { "arsham/arshlib.nvim" },
   event = { "VeryLazy" },
-  config = function()
-    require("listish").config({})
-  end,
+  config = true,
 }
