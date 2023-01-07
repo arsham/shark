f181254d33589c070568b36054233fe063e5d571
Arsham Shirvani <arshamshirvani@gmail.com>
2023-01-07 23:00:39 +0000

    ref(inc_rename): streamline the spec

--- lua/plugins/inc_rename.lua
+++ lua/plugins/inc_rename.lua
@@ -1,9 +1,7 @@
 return {
   "smjonas/inc-rename.nvim",
-  config = function()
-    require("inc_rename").setup({
-      cmd_name = "Rename",
-    })
-  end,
   event = { "LspAttach" },
+  config = {
+    cmd_name = "Rename",
+  },
 }
