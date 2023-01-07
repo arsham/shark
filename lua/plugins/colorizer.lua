96c8c2339e0119ddb9b08a1d83254dbaf6cb1081
Arsham Shirvani <arshamshirvani@gmail.com>
2023-01-07 22:02:25 +0000

    ref(colorizer): streamline the spec

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,11 +1,9 @@
 local colorizer_ft = { "css", "scss", "sass", "html", "lua", "markdown", "norg" }
 return {
   "NvChad/nvim-colorizer.lua",
-  config = function()
-    require("colorizer").setup({
-      filetypes = colorizer_ft,
-    })
-  end,
   ft = colorizer_ft,
   cond = require("util").full_start,
+  config = {
+    filetypes = colorizer_ft,
+  },
 }
