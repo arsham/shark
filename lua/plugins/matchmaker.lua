260dabdc333f1156a9135b8d9da77c2ab6af3a8d
Arsham Shirvani <arshamshirvani@gmail.com>
2023-01-07 23:26:23 +0000

    ref(matchmaker): streamline the spec

--- lua/plugins/matchmaker.lua
+++ lua/plugins/matchmaker.lua
@@ -5,9 +5,7 @@ return {
     "junegunn/fzf",
     "junegunn/fzf.vim",
   },
-  config = function()
-    require("matchmaker").config({})
-  end,
   keys = { "<leader>me", "<leader>ma", "<leader>ml", "<leader>mp" },
   cond = require("util").full_start,
+  config = true,
 }
