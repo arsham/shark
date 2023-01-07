c29c2b019592faa764e7645009b79d9223586878
Arsham Shirvani <arshamshirvani@gmail.com>
2023-01-07 23:38:16 +0000

    ref(navigator): streamline the spec

--- lua/plugins/navigator.lua
+++ lua/plugins/navigator.lua
@@ -1,15 +1,11 @@
 return {
   "numToStr/Navigator.nvim",
-  config = function()
-    local navigator = require("Navigator")
-    navigator.setup()
-
-    -- stylua: ignore start
-    vim.keymap.set("n", "<C-h>", navigator.left, { silent = true, desc = "navigate to left window or tmux pane" })
-    vim.keymap.set("n", "<C-k>", navigator.up, { silent = true, desc = "navigate to upper window or tmux pane" })
-    vim.keymap.set("n", "<C-l>", navigator.right, { silent = true, desc = "navigate to right window or tmux pane" })
-    vim.keymap.set("n", "<C-j>", navigator.down, { silent = true, desc = "navigate to lower window or tmux pane" })
-    -- stylua: ignore end
-  end,
-  keys = { "<C-h>", "<C-j>", "<C-k>", "<C-l>" },
+  config = true,
+  -- stylua: ignore
+  keys = {
+    { mode = "n", "<C-h>", function() require("Navigator").left() end,  { silent = true, desc = "navigate to left window or tmux pane" } },
+    { mode = "n", "<C-k>", function() require("Navigator").up() end,    { silent = true, desc = "navigate to upper window or tmux pane" } },
+    { mode = "n", "<C-l>", function() require("Navigator").right() end, { silent = true, desc = "navigate to right window or tmux pane" } },
+    { mode = "n", "<C-j>", function() require("Navigator").down() end,  { silent = true, desc = "navigate to lower window or tmux pane" } },
+  },
 }
