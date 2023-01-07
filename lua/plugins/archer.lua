5dabf558db78bcbb722ce01d70413dd626775f01
Arsham Shirvani <arshamshirvani@gmail.com>
2023-01-07 20:33:28 +0000

    ref(archer): use a table for configuration

--- lua/plugins/archer.lua
+++ lua/plugins/archer.lua
@@ -1,8 +1,6 @@
 return {
   "arsham/archer.nvim",
   dependencies = { "arsham/arshlib.nvim" },
-  config = function()
-    require("archer").config({})
-  end,
   event = { "VeryLazy" },
+  config = true,
 }
