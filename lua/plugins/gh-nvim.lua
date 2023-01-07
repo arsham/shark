d3f47712d0f631c9f348b8b3d358f94f3c951a34
Arsham Shirvani <arshamshirvani@gmail.com>
2023-01-07 22:56:51 +0000

    ref(gh.nvim): streamline the spec

--- lua/plugins/gh-nvim.lua
+++ lua/plugins/gh-nvim.lua
@@ -1,43 +1,43 @@
-local function config()
-  require("litee.lib").setup({
-    tree = {
-      icon_set = "nerd",
-    },
-    panel = {
-      orientation = "left",
-      panel_size = 30,
-    },
-    notify = {
-      enabled = false,
-    },
-  })
-
-  require("litee.lib").setup()
-  require("litee.gh").setup({
-    icon_set = "nerd",
-    refresh_interval = 60000, -- one minute
-    keymaps = {
-      open = "<CR>",
-      expand = "zo", -- expand a collapsed node
-      collapse = "zc", -- collpased an expanded node
-      goto_issue = "gd",
-      submit_comment = "<C-s>",
-      actions = "<C-a>",
-      resolve_thread = "<C-r>",
-      goto_web = "gx",
-      select = "<leader>ss",
-      clear_selection = "<leader>sc",
-      toggle_unread = "u",
-    },
-  })
-end
-
 return {
-  "ldelossa/gh.nvim",
-  config = config,
-  dependencies = {
-    "ldelossa/litee.nvim",
-    "ibhagwan/fzf-lua",
+  {
+    "ldelossa/gh.nvim",
+    config = {
+      icon_set = "nerd",
+      refresh_interval = 60000, -- one minute
+      keymaps = {
+        open = "<CR>",
+        expand = "zo", -- expand a collapsed node
+        collapse = "zc", -- collpased an expanded node
+        goto_issue = "gd",
+        submit_comment = "<C-s>",
+        actions = "<C-a>",
+        resolve_thread = "<C-r>",
+        goto_web = "gx",
+        select = "<leader>ss",
+        clear_selection = "<leader>sc",
+        toggle_unread = "u",
+      },
+    },
+    dependencies = {
+      "ibhagwan/fzf-lua",
+      {
+        "ldelossa/litee.nvim",
+        config = function()
+          require("litee.lib").setup({
+            tree = {
+              icon_set = "nerd",
+            },
+            panel = {
+              orientation = "left",
+              panel_size = 30,
+            },
+            notify = {
+              enabled = false,
+            },
+          })
+        end,
+      },
+    },
+    cmd = "GH",
   },
-  cmd = "GH",
 }
