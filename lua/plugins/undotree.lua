218363bea4a64a1b08f483206e9a561003092776
Arsham Shirvani <arshamshirvani@gmail.com>
2023-01-08 00:59:55 +0000

    ref(undotree): streamline the spec

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,11 +1,12 @@
 return {
   "mbbill/undotree",
   branch = "search",
-  config = function()
+  init = function()
     vim.g.undotree_CustomUndotreeCmd = "vertical 40 new"
     vim.g.undotree_CustomDiffpanelCmd = "botright 15 new"
-    vim.keymap.set("n", "<leader>u", ":UndotreeToggle<CR>", { silent = true })
   end,
   cmd = { "UndotreeShow", "UndotreeToggle" },
-  keys = { "<leader>u" },
+  keys = {
+    { mode = "n", "<leader>u", ":UndotreeToggle<CR>", { silent = true } },
+  },
 }
