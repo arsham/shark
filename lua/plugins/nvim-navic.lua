c745a69fd2c188e838d6c8df73e5fe462c1592b0
Arsham Shirvani <arshamshirvani@gmail.com>
2023-01-08 00:18:13 +0000

    style(navic): align tables in the code

--- lua/plugins/nvim-navic.lua
+++ lua/plugins/nvim-navic.lua
@@ -1,76 +1,76 @@
 local function config()
+  vim.g.navic_silence = true
   local navic = require("nvim-navic")
   local separator = " %#CmpItemKindDefault#▶ %*"
 
--- stylua: ignore start
-local icons = { --{{{
-  array         = " ",
-  boolean       = "◩ ",
-  calendar      = " ",
-  class         = " ",
-  constant      = " ",
-  constructor   = " ",
-  container     = " ",
-  enum          = "練",
-  enumMember    = " ",
-  event         = " ",
-  field         = " ",
-  file          = " ",
-  func          = " ",
-  interface     = "練",
-  key           = " ",
-  method        = " ",
-  module        = " ",
-  namespace     = " ",
-  null          = "ﳠ ",
-  number        = " ",
-  object        = " ",
-  operator      = " ",
-  package       = " ",
-  property      = " ",
-  string        = " ",
-  struct        = " ",
-  table         = " ",
-  tag           = " ",
-  typeParameter = " ",
-  variable      = " ",
-  watch         = " ",
-} --}}}
+  -- stylua: ignore
+  local icons = { --{{{
+    array         = " ",
+    boolean       = "◩ ",
+    calendar      = " ",
+    class         = " ",
+    constant      = " ",
+    constructor   = " ",
+    container     = " ",
+    enum          = "練",
+    enumMember    = " ",
+    event         = " ",
+    field         = " ",
+    file          = " ",
+    func          = " ",
+    interface     = "練",
+    key           = " ",
+    method        = " ",
+    module        = " ",
+    namespace     = " ",
+    null          = "ﳠ ",
+    number        = " ",
+    object        = " ",
+    operator      = " ",
+    package       = " ",
+    property      = " ",
+    string        = " ",
+    struct        = " ",
+    table         = " ",
+    tag           = " ",
+    typeParameter = " ",
+    variable      = " ",
+    watch         = " ",
+  } --}}}
 
-vim.g.navic_silence = true
-navic.setup({ --{{{
-  icons = {
-    Array         = icons.array,
-    Boolean       = icons.boolean,
-    Class         = icons.class,
-    Constant      = icons.constant,
-    Constructor   = icons.constructor,
-    Enum          = icons.enum,
-    EnumMember    = icons.enumMember,
-    Event         = icons.event,
-    Field         = icons.field,
-    File          = icons.file,
-    Function      = icons.func,
-    Interface     = icons.interface,
-    Key           = icons.key,
-    Method        = icons.method,
-    Module        = icons.module,
-    Namespace     = icons.namespace,
-    Null          = icons.null,
-    Number        = icons.number,
-    Object        = icons.object,
-    Operator      = icons.operator,
-    Package       = icons.package,
-    Property      = icons.property,
-    String        = icons.string,
-    Struct        = icons.struct,
-    TypeParameter = icons.typeParameter,
-    Variable      = icons.variable,
-  },
-  separator = separator,
-  highlight = true,
-}) --}}}
-  -- stylua: ignore end
+  -- stylua: ignore
+  navic.setup({ --{{{
+    icons = {
+      Array         = icons.array,
+      Boolean       = icons.boolean,
+      Class         = icons.class,
+      Constant      = icons.constant,
+      Constructor   = icons.constructor,
+      Enum          = icons.enum,
+      EnumMember    = icons.enumMember,
+      Event         = icons.event,
+      Field         = icons.field,
+      File          = icons.file,
+      Function      = icons.func,
+      Interface     = icons.interface,
+      Key           = icons.key,
+      Method        = icons.method,
+      Module        = icons.module,
+      Namespace     = icons.namespace,
+      Null          = icons.null,
+      Number        = icons.number,
+      Object        = icons.object,
+      Operator      = icons.operator,
+      Package       = icons.package,
+      Property      = icons.property,
+      String        = icons.string,
+      Struct        = icons.struct,
+      TypeParameter = icons.typeParameter,
+      Variable      = icons.variable,
+    },
+    separator = separator,
+    highlight = true,
+  }) --}}}
 
   local cache = {}
   local function get_name(filename) --{{{
@@ -93,10 +93,6 @@ navic.setup({ --{{{
   local bar = { --{{{
     provider = function()
       local filename = vim.fn.expand("%:t")
-      if filename == "NvimTree_1" then
-        return "Nvim Tree"
-      end
-
       local ret = get_name(filename)
       local extra = ""
       if navic.is_available() then
