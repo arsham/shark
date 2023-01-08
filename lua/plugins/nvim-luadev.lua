bf275ab7722f9d6d65624d4cde1fd0b9b48d7308
Arsham Shirvani <arshamshirvani@gmail.com>
2023-01-08 00:06:55 +0000

    ref(luadev): streamline the spec

--- lua/plugins/nvim-luadev.lua
+++ lua/plugins/nvim-luadev.lua
@@ -1,32 +1,12 @@
-local function config()
-  vim.keymap.set(
-    "n",
-    "<leader>ll",
-    "<Plug>(Luadev-RunLine)",
-    { silent = true, desc = "Execute the current line" }
-  )
-  vim.keymap.set(
-    "n",
-    "<leader>la",
-    "<Plug>(Luadev-Run)",
-    { silent = true, desc = "Operator to execute lua code over a movement or text object." }
-  )
-  vim.keymap.set(
-    "n",
-    "<leader>le",
-    "<Plug>(Luadev-RunWord)",
-    { silent = true, desc = "Eval identifier under cursor, including table.attr" }
-  )
-  -- vim.keymap.set(
-  --   "<leader>ll",
-  --   "<Plug>(Luadev-Complete)",
-  --   { silent = true, desc = "in insert mode: complete (nested) global table fields" }
-  -- )
-end
-
 return {
   "bfredl/nvim-luadev",
-  config = config,
   cmd = { "Luadev" },
   cond = require("util").full_start_with_lsp,
+  -- stylua: ignore
+  keys = {
+    { mode = "n", "<leader>ll", "<Plug>(Luadev-RunLine)",     { silent = true, desc = "Execute the current line" }, },
+    { mode = "n", "<leader>la", "<Plug>(Luadev-Run)",         { silent = true, desc = "Operator to execute lua code over a movement or text object." }, },
+    { mode = "n", "<leader>le", "<Plug>(Luadev-RunWord)",     { silent = true, desc = "Eval identifier under cursor, including table.attr" }, },
+    -- { mode = "n", "<leader>ll", "<Plug>(Luadev-Complete)", { silent = true, desc = "in insert mode: complete (nested) global table fields" }, },
+  },
 }
