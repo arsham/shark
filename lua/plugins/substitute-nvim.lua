cd7edcdc198fe51d8ca01f04a7c373331eef96ee
Arsham Shirvani <arshamshirvani@gmail.com>
2023-01-08 00:22:44 +0000

    ref(substitute-nvim): streamline the spec

--- lua/plugins/substitute-nvim.lua
+++ lua/plugins/substitute-nvim.lua
@@ -1,22 +1,15 @@
+local function exchange()
+  return require("substitute.exchange")
+end
+
 return {
   "gbprod/substitute.nvim",
-  config = function()
-    require("substitute").setup({})
-
-    local exchange = require("substitute.exchange")
-
-    vim.keymap.set("n", "cx", function()
-      exchange.operator()
-    end, { noremap = true, desc = "exchange operator" })
-    vim.keymap.set("n", "cxx", function()
-      exchange.line()
-    end, { noremap = true, desc = "exchange the line" })
-    vim.keymap.set("x", "X", function()
-      exchange.visual()
-    end, { noremap = true, desc = "exchange operator" })
-    vim.keymap.set("n", "cxc", function()
-      exchange.cancel()
-    end, { noremap = true, desc = "cancel exchange" })
-  end,
-  keys = { { "cx", "cxx", mode = "n" }, { "X", mode = "v" } },
+  -- stylua: ignore
+  keys = {
+    { mode = "n", "cx",  function() exchange().operator() end, { noremap = true, desc = "exchange operator" }},
+    { mode = "n", "cxx", function() exchange().line() end,     { noremap = true, desc = "exchange the line" }},
+    { mode = "x", "X",   function() exchange().visual() end,   { noremap = true, desc = "exchange operator" }},
+    { mode = "n", "cxc", function() exchange().cancel() end,   { noremap = true, desc = "cancel exchange" }},
+  },
+  config = true,
 }
