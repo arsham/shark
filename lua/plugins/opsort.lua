a4915c484f92ce6b6f9a62c51bb53733cb30b102
Arsham Shirvani <arshamshirvani@gmail.com>
2023-01-08 00:19:23 +0000

    ref(opsort): streamline the spec

--- lua/plugins/opsort.lua
+++ lua/plugins/opsort.lua
@@ -3,10 +3,8 @@ return {
   init = function()
     vim.g.opsort_no_mappings = true
   end,
-  config = function()
-    vim.keymap.set("x", "gso", "<plug>Opsort", { silent = true })
-    vim.keymap.set("n", "gso", "<plug>Opsort", { silent = true })
-    vim.keymap.set("n", "gsoo", "<plug>OpsortLines", { silent = true })
-  end,
-  event = { "BufReadPost", "BufNewFile", "InsertEnter" },
+  keys = {
+    { mode = { "n", "x" }, "gso", "<plug>Opsort", { silent = true } },
+    { mode = "n", "gsoo", "<plug>OpsortLines", { silent = true } },
+  },
 }
