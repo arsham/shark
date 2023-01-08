5436449ada0a64be78511af7bd9821c18eb68318
Arsham Shirvani <arshamshirvani@gmail.com>
2023-01-08 00:20:54 +0000

    ref(sj): streamline the spec

--- lua/plugins/sj_nvim.lua
+++ lua/plugins/sj_nvim.lua
@@ -1,24 +1,23 @@
 return {
   "woosaaahh/sj.nvim",
   event = { "VeryLazy" },
-  config = function()
-    local sj = require("sj")
-    sj.setup({
-      pattern_type = "vim", -- lua_plain, lua, vim, vim_very_magic
-      prompt_prefix = "🧲 ",
-      relative_labels = true,
-      separator = "",
+  -- stylua: ignore
+  keys = {
+    { mode = "n", "<leader><leader>", function() require("sj").run() end, { desc = "Initiate SJ" }},
+  },
+  config = {
+    pattern_type = "vim", -- lua_plain, lua, vim, vim_very_magic
+    prompt_prefix = "🧲 ",
+    relative_labels = true,
+    separator = "",
 
-      keymaps = {
-        validate = "<CR>",
-        prev_match = "<A-,>",
-        next_match = "<A-;>",
-        prev_pattern = "<C-p>",
-        next_pattern = "<C-n>",
-        send_to_qflist = "<A-q>",
-      },
-    })
-
-    vim.keymap.set("n", "<leader><leader>", sj.run, { desc = "Initiate SJ" })
-  end,
+    keymaps = {
+      validate = "<CR>",
+      prev_match = "<A-,>",
+      next_match = "<A-;>",
+      prev_pattern = "<C-p>",
+      next_pattern = "<C-n>",
+      send_to_qflist = "<A-q>",
+    },
+  },
 }
