baf63a65bdbe64b13a1b4b6a40e18b829daac0ea
Arsham Shirvani <arshamshirvani@gmail.com>
2023-03-12 17:16:46 +0000

    ref(indent-tools): load only on buffer load

--- lua/plugins/indent_tools.lua
+++ lua/plugins/indent_tools.lua
@@ -1,7 +1,7 @@
 return {
   "arsham/indent-tools.nvim",
   dependencies = { "arsham/arshlib.nvim" },
-  event = { "VeryLazy" },
+  event = { "BufReadPost", "BufNewFile" },
   cond = require("util").full_start,
   config = true,
 }
