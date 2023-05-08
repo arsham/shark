ce0c397475187de046a99def2fb1efbe25535c54
Arsham Shirvani <arshamshirvani@gmail.com>
2023-05-08 20:58:57 +0100

    feat(treesitter): make mappings repeatable

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -155,6 +155,18 @@ local function config()
     autopairs = { enable = true },
     matchup = { enable = true },
   })
+
+  local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+
+  -- Repeat movement with ; and ,
+  vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+  vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+
+  -- Make builtin f, F, t, T also repeatable with ; and ,
+  vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
+  vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
+  vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
+  vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
 end
 
 return {
