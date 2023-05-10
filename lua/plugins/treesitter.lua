d2c78526549c3d953249a08cfd80392f9708876f
Arsham Shirvani <arshamshirvani@gmail.com>
2023-05-10 22:20:05 +0100

    fix(treesitter): fix dot operator
    Apparently the builtin repeat setup makes the dot operator useless.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -161,12 +161,6 @@ local function config()
   -- Repeat movement with ; and ,
   vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
   vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
-
-  -- Make builtin f, F, t, T also repeatable with ; and ,
-  vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
-  vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
-  vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
-  vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
 end
 
 return {
