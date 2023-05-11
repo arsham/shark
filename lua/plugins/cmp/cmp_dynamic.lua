57a1b8ab85106c41f94b4188e5f33dde15ab5613
Arsham Shirvani <arshamshirvani@gmail.com>
2023-05-11 23:30:43 +0100

    ref(cmp_dynamic): fix deprecated setup

--- lua/plugins/cmp/cmp_dynamic.lua
+++ lua/plugins/cmp/cmp_dynamic.lua
@@ -6,22 +6,16 @@ return {
     require("cmp_dynamic").register({
       {
         label = "today",
-        insertText = 1,
-        cb = {
-          function()
-            return os.date("%Y/%m/%d")
-          end,
-        },
+        insertText = function()
+          return os.date("%Y/%m/%d")
+        end,
       },
       {
         label = "yesterday",
-        insertText = 1,
         resolve = true,
-        cb = {
-          function()
-            return Date.new():add_date(-1):day(1):format("%Y/%m/%d")
-          end,
-        },
+        insertText = function()
+          return Date.new():add_date(-1):day(1):format("%Y/%m/%d")
+        end,
       },
     })
   end,
