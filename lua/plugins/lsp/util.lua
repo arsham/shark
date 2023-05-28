b6c8db556125d2b2f33c9dbdbbe5174552a74cb6
Arsham Shirvani <arshamshirvani@gmail.com>
2023-05-28 22:02:49 +0100

    feat(lsp): reload rust workspace on cargo updates

--- lua/plugins/lsp/util.lua
+++ lua/plugins/lsp/util.lua
@@ -339,6 +339,42 @@ function M.support_commands() --{{{
   nnoremap("<localleader>dr", restart_lsp, "Restart LSP server")
 end --}}}
 
+local handler = function(err)
+  if err then
+    local msg = string.format("Error reloading Rust workspace: %v", err)
+    vim.notify(msg, vim.lsp.log_levels.ERROR, {
+      title = "Reloading Rust workspace",
+      timeout = 3000,
+    })
+  else
+    vim.notify("Workspace has been reloaded")
+  end
+end
+
+local function reload_rust_workspace()
+  local clients = vim.lsp.get_active_clients()
+  for _, client in ipairs(clients) do
+    if client.name == "rust_analyzer" then
+      client.request("rust-analyzer/reloadWorkspace", nil, handler, 0)
+    end
+  end
+end
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  pattern = "*.rs",
+  callback = function()
+    quick.buffer_command("ReloadWorkspace", function()
+      vim.lsp.buf_request(0, "rust-analyzer/reloadWorkspace", nil, handler)
+    end, { range = true })
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  group = augroup("reload_rust_workspace"),
+  pattern = "*/Cargo.toml",
+  callback = reload_rust_workspace,
+})
+
 return M
 
 -- vim: fdm=marker fdl=0
