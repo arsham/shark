5fe9dad56b68dfbda04e4b982cd33b2da9592155
Arsham Shirvani <arshamshirvani@gmail.com>
2023-05-29 00:21:39 +0100

    feat(queries): add local treesitter queries

---
+++ after/queries/go/locals.scm
@@ -0,0 +1,17 @@
+;; extends
+
+(var_spec) @scope
+
+(field_declaration
+  name: (field_identifier) @definition.field)
+
+(method_declaration
+  name: (field_identifier) @method.name
+  parameters: (parameter_list) @method.parameter_list) @interface.method.declaration
+
+(type_declaration
+  (type_spec
+    name: (type_identifier) @name
+    type: [(struct_type) (interface_type)] @type)) @start
+
+;; vim: fo-=t
