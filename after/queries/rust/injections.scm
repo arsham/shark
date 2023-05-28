ba51162215a5d2058c312151100f32473c46c998
Arsham Shirvani <arshamshirvani@gmail.com>
2023-05-29 00:25:25 +0100

    feat(queries): add rust injections queries

---
+++ after/queries/rust/injections.scm
@@ -0,0 +1,11 @@
+;; extends
+(
+    (line_comment) @_first
+    (_) @rust
+    (line_comment) @_last
+    (#match? @_first "^/// ```$")
+    (#match? @_last "^/// ```$")
+    (#offset! @rust 0 4 0 4)
+)
+
+;; vim: fo-=t
