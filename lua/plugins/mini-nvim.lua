4426ed2908ed4edcd564741c2998df6cc25f7d0f
Arsham Shirvani <arshamshirvani@gmail.com>
2023-05-29 18:35:21 +0100

    feat(plugin): add mini.align

--- lua/plugins/mini-nvim.lua
+++ lua/plugins/mini-nvim.lua
@@ -60,6 +60,38 @@ return {
     cond = require("config.util").should_start("echasnovski/mini.trailspace"),
     enabled = require("config.util").is_enabled("echasnovski/mini.trailspace"),
   }, -- }}}
+
+  { -- mini.align {{{
+    "echasnovski/mini.align",
+    event = { "BufRead", "BufNewFile" },
+    config = function()
+      require("mini.align").setup({
+        mappings = {
+          start = "ga",
+          start_with_preview = "gA",
+        },
+
+        -- Default options controlling alignment process
+        options = {
+          split_pattern = "",
+          justify_side = "left",
+          merge_delimiter = "",
+        },
+
+        -- Default steps performing alignment (if `nil`, default is used)
+        steps = {
+          pre_split = {},
+          split = nil,
+          pre_justify = {},
+          justify = nil,
+          pre_merge = {},
+          merge = nil,
+        },
+      })
+    end,
+    cond = require("config.util").should_start("echasnovski/mini.align"),
+    enabled = require("config.util").is_enabled("echasnovski/mini.align"),
+  }, -- }}}
 }
 
 -- vim: fdm=marker fdl=0
