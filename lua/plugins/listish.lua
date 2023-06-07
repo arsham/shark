6dfe3f6f132f9e23e1977ae051874bbdd4af6f2e
Arsham Shirvani <arshamshirvani@gmail.com>
2023-06-07 07:40:29 +0100

    ref(listish): load on buffer events
    The plugin has a soft dependency on treesitter so we have to load after.

--- lua/plugins/listish.lua
+++ lua/plugins/listish.lua
@@ -1,8 +1,11 @@
 return {
   "arsham/listish.nvim",
   name = "listish.nvim",
-  dependencies = { "arshlib.nvim" },
-  event = { "VeryLazy" },
+  dependencies = {
+    "arshlib.nvim",
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+  event = { "BufReadPre", "BufNewFile" },
   config = true,
   enabled = require("config.util").is_enabled("arsham/listish.nvim"),
 }
