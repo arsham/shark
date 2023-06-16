cb208cab4a239522a044cd2c828fee1dc62baba2
Arsham Shirvani <arshamshirvani@gmail.com>
2023-06-16 21:45:38 +0100

    ref(help,ftplugin): use set option API

--- after/ftplugin/help.lua
+++ after/ftplugin/help.lua
@@ -1,4 +1,4 @@
-vim.wo.conceallevel = 0
+vim.api.nvim_set_option_value("conceallevel", 0, { scope = "local", win = 0 })
 vim.schedule(function()
   vim.bo.syntax = "help"
   pcall(vim.treesitter.start)
