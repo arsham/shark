66860f416171b2a76dd8a783cd1fc4455581e20f
Arsham Shirvani <arshamshirvani@gmail.com>
2023-06-16 21:46:00 +0100

    ref(json,ftplugin): use set option API

--- after/ftplugin/json.lua
+++ after/ftplugin/json.lua
@@ -1,5 +1,5 @@
 vim.bo.softtabstop = 2
 vim.bo.tabstop = 2
 vim.bo.shiftwidth = 2
-vim.wo.foldmethod = "indent"
-vim.wo.foldlevel = 2
+vim.api.nvim_set_option_value("foldmethod", "indent", { scope = "local", win = 0 })
+vim.api.nvim_set_option_value("foldlevel", 2, { scope = "local", win = 0 })
