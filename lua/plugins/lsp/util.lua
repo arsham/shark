25478d89533210fce1b0ad14e3b5f698bb6eef4e
Arsham Shirvani <arshamshirvani@gmail.com>
2024-05-18 10:09:46 +0100

    ref(lsp): ignore mock files when jump to impl

--- lua/plugins/lsp/util.lua
+++ lua/plugins/lsp/util.lua
@@ -280,7 +280,20 @@ end --}}}
 
 function M.implementation() --{{{
   local perform = function()
-    fzf.lsp_implementations({ jump_to_single_result = true })
+    local check = "_mock.go"
+    local filter = function(items)
+      local ret = {}
+      for _, item in ipairs(items) do
+        if item.filename:sub(-#check) ~= check then
+          table.insert(ret, item)
+        end
+      end
+      return ret
+    end
+    fzf.lsp_implementations({
+      jump_to_single_result = true,
+      filter = filter,
+    })
   end
   quick.buffer_command("Implementation", perform)
   nnoremap("<localleader>gi", perform, "Go to implementation")
