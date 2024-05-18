15b12bfe15ea640fe3cf91fc943c57d763970234
Arsham Shirvani <arshamshirvani@gmail.com>
2024-05-18 09:34:53 +0100

    fix(abbr): schedule abbreviations setup

--- lua/config/abbreviations.lua
+++ lua/config/abbreviations.lua
@@ -1,2 +1,17 @@
-vim.cmd.iabbrev("teh", "the")
-vim.cmd.iabbrev("Teh", "The")
+local spec = {
+  teh = "the",
+  Teh = "The",
+}
+local fn = function(term, abbr)
+  vim.cmd.iabbrev(term, abbr)
+end
+if vim.fn.has("nvim-0.10.0") == 1 then
+  fn = function(term, abbr)
+    vim.keymap.set("ia", term, abbr)
+  end
+end
+vim.schedule(function()
+  for term, abbr in pairs(spec) do
+    fn(term, abbr)
+  end
+end)
