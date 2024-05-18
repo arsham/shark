807c19c4229f091264fe2a7f8a68f60f4ff59c23
Arsham Shirvani <arshamshirvani@gmail.com>
2024-05-18 10:14:33 +0100

    fix(tree-sitter): don't add keymaps in op mode

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -77,14 +77,14 @@ return {
     end
 
     -- Repeat movement with ; and ,
-    vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
-    vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+    vim.keymap.set({ "n", "x" }, ";", ts_repeat_move.repeat_last_move)
+    vim.keymap.set({ "n", "x" }, ",", ts_repeat_move.repeat_last_move_opposite)
 
     -- Make builtin f, F, t, T also repeatable with ; and ,
-    vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
-    vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
-    vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
-    vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
+    vim.keymap.set({ "n", "x" }, "f", ts_repeat_move.builtin_f)
+    vim.keymap.set({ "n", "x" }, "F", ts_repeat_move.builtin_F)
+    vim.keymap.set({ "n", "x" }, "t", ts_repeat_move.builtin_t)
+    vim.keymap.set({ "n", "x" }, "T", ts_repeat_move.builtin_T)
   end,
   enabled = require("config.util").is_enabled("nvim-treesitter/nvim-treesitter"),
 }
