fe9b221d7ab3ca89a5c4d1c5614eab388ec8c5f7
Arsham Shirvani <arshamshirvani@gmail.com>
2024-05-18 09:51:20 +0100

    fix(indent-tools): lazy load on operator mode too

--- lua/plugins/indent_tools.lua
+++ lua/plugins/indent_tools.lua
@@ -5,7 +5,12 @@ return {
     "arshlib.nvim",
     "nvim-treesitter/nvim-treesitter-textobjects",
   },
-  keys = { "]i", "[i", { mode = "v", "ii" }, { mode = "o", "ii" } },
+  keys = {
+    "]i",
+    "[i",
+    { mode = { "v", "x", "o" }, "ii" },
+    { mode = { "v", "x", "o" }, "ai" },
+  },
   config = true,
   enabled = require("config.util").is_enabled("arsham/indent-tools.nvim"),
 }
