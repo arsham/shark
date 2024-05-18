1b8a8eec78c66b36210eaf0ce634857222e8c7e7
Arsham Shirvani <arshamshirvani@gmail.com>
2024-05-18 10:12:15 +0100

    ref(neo-tree): upgrade to v3 branch

--- lua/plugins/neo-tree_nvim.lua
+++ lua/plugins/neo-tree_nvim.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  branch = "v2.x",
+  branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
@@ -204,7 +204,7 @@ return {
           "target",
         },
       },
-      follow_current_file = true,
+      follow_current_file = { enabled = true },
       use_libuv_file_watcher = true,
     }, -- }}}
   },
