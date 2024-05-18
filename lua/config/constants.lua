107c25034494e4740daa0100c033638fede637d9
Arsham Shirvani <arshamshirvani@gmail.com>
2024-05-18 11:49:03 +0100

    fix(constants): indent max size name

--- lua/config/constants.lua
+++ lua/config/constants.lua
@@ -3,5 +3,6 @@ return {
   large_file_size = 1024 * 1024 * 5,
   treesitter_highlight_maxlines = 12 * 1024,
   treesitter_highlight_max_filesize = 200 * 1024, -- 200 KB
+  treesitter_indent_max_filesize = 5 * 1024, -- 5 KB
   treesitter_refactor_maxlines = 10 * 1024,
 }
