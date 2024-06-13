51da0d159214dee4133f712652efb5a3abb23513
Arsham Shirvani <arshamshirvani@gmail.com>
2024-06-13 08:00:23 +0100

    ref(comment): use ts_context_commentstring hooks

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -7,24 +7,7 @@ return {
   keys = { { "gc", mode = { "n", "v" } } },
   opts = {
     pre_hook = function(ctx)
-      local cmt_utils = require("Comment.utils")
-      local ts_utils = require("ts_context_commentstring.utils")
-      local internal = require("ts_context_commentstring.internal")
-      -- Determine the location where to calculate commentstring from
-      local location = nil
-      if ctx.ctype == cmt_utils.ctype.block then
-        location = ts_utils.get_cursor_location()
-      elseif ctx.cmotion == cmt_utils.cmotion.v or ctx.cmotion == cmt_utils.cmotion.V then
-        location = ts_utils.get_visual_start_location()
-      end
-
-      -- Detemine whether to use linewise or blockwise commentstring
-      local type = ctx.ctype == cmt_utils.ctype.line and "__default" or "__multiline"
-
-      return internal.calculate_commentstring({
-        key = type,
-        location = location,
-      })
+      return require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()(ctx)
     end,
     enabled = require("config.util").is_enabled("numToStr/Comment.nvim"),
   },
