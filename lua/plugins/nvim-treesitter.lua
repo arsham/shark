24b107411cff25eb44305a5a7ad87ac59be983de
Arsham Shirvani <arshamshirvani@gmail.com>
2024-06-13 08:06:00 +0100

    ref(treesitter): update f and t setup to newest

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -90,10 +90,10 @@ return {
     vim.keymap.set({ "n", "x" }, ",", ts_repeat_move.repeat_last_move_opposite)
 
     -- Make builtin f, F, t, T also repeatable with ; and ,
-    vim.keymap.set({ "n", "x" }, "f", ts_repeat_move.builtin_f)
-    vim.keymap.set({ "n", "x" }, "F", ts_repeat_move.builtin_F)
-    vim.keymap.set({ "n", "x" }, "t", ts_repeat_move.builtin_t)
-    vim.keymap.set({ "n", "x" }, "T", ts_repeat_move.builtin_T)
+    vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+    vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+    vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+    vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
   end,
   enabled = require("config.util").is_enabled("nvim-treesitter/nvim-treesitter"),
 }
