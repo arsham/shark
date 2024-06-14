3f4a5f2d9fd56219b1051a9dccd2f639e755e729
Arsham Shirvani <arshamshirvani@gmail.com>
2024-06-14 18:21:56 +0100

    fix(lsp): don't run actions on organise imports

--- lua/plugins/lsp/on_attach.lua
+++ lua/plugins/lsp/on_attach.lua
@@ -436,25 +436,20 @@ local function lsp_organise_imports() --{{{
   local context = { source = { organizeImports = true } }
   vim.validate({ context = { context, "table", true } })
 
-  local params = vim.lsp.util.make_range_params()
-  params.context = context
-
-  local method = "textDocument/codeAction"
-  local timeout = 1000 -- ms
-
-  local ok, resp = pcall(vim.lsp.buf_request_sync, 0, method, params, timeout)
-  if not ok or not resp then
-    return
-  end
-
   for _, client in ipairs(get_clients()) do
-    local offset_encoding = client.offset_encoding or "utf-16"
-    if client.id and resp[client.id] then
-      local result = resp[client.id].result
-      if result and result[1] and result[1].edit then
-        local edit = result[1].edit
-        if edit then
-          vim.lsp.util.apply_workspace_edit(result[1].edit, offset_encoding)
+    local params = vim.lsp.util.make_range_params(nil, client.offset_encoding)
+    params.context = { only = { "source.organizeImports" } }
+
+    local timeout = 1000 -- ms
+    local resp = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout)
+    for _, res in pairs(resp or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          -- Apply the workspace edit using the correct offset encoding.
+          vim.lsp.util.apply_workspace_edit(r.edit, client.offset_encoding)
+        else
+          -- Execute the command if there is no edit.
+          vim.lsp.buf.execute_command(r.command)
         end
       end
     end
